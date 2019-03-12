414e9c091bcea8fa71454088376acc18252bf497
kimduong2512 <duonglk2014@gmail.com>
2019-03-12 18:58:49 +0700

    Duong push project

---
+++ Source/AirlinesReservationSystem/AirlinesReservationSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirlinesReservationSystem.Startup))]
+namespace AirlinesReservationSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
